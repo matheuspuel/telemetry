1bc697fb11659a16428afa3f20beb048019a00e0
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-06-17 18:00:57 -0300

    fix missing dependency

--- src/utils/Error.ts
+++ src/utils/Error.ts
@@ -1,34 +1,15 @@
 import { absurd } from 'fp-ts/function'
 import * as D from 'io-ts/Decoder'
-import mysql from 'mysql2'
 import { HttpStatus } from '../infra/http/utils'
 import { Failure, failure } from './Result'
 
 /**
  * Union of all errors returned from the endpoints
  */
-export type EndpointError =
-  | RequestValidationError
-  | DatabaseQueryError
-  | DatabaseDecodingError
-  | ResourceNotFoundError
+export type EndpointError = RequestValidationError | ResourceNotFoundError
 
 export class RequestValidationError extends Error {
   public type = 'RequestValidationError' as const
-  constructor(public error: D.DecodeError) {
-    super()
-  }
-}
-
-export class DatabaseQueryError extends Error {
-  public type = 'DatabaseQueryError' as const
-  constructor(public error: mysql.QueryError) {
-    super()
-  }
-}
-
-export class DatabaseDecodingError extends Error {
-  public type = 'DatabaseDecodingError' as const
   constructor(public error: D.DecodeError) {
     super()
   }
@@ -52,14 +33,6 @@ export const handleError = (
       const message = D.draw(error.error)
       console.warn('Request validation error:', message)
       return failure({ status: HttpStatus.INTERNAL_SERVER_ERROR, message })
-    }
-    case 'DatabaseQueryError': {
-      console.error('Database error:', error)
-      return failure({ status: HttpStatus.INTERNAL_SERVER_ERROR })
-    }
-    case 'DatabaseDecodingError': {
-      console.error('Database decoding error:', D.draw(error.error))
-      return failure({ status: HttpStatus.INTERNAL_SERVER_ERROR })
     }
     case 'ResourceNotFoundError': {
       return failure({ status: HttpStatus.NOT_FOUND })
